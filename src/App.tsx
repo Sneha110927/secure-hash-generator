d863be9ffed30422238714e07b9c5622eb309d33
Sneha Jhawar <jhawarsneha11@gmail.com>
2026-06-08 02:51:06 +0530

    Fix ChatBot casing conflict

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Features from "./components/Features";
 import Algorithms from "./components/Algorithms";
 import Stats from "./components/Stats";
 import Footer from "./components/Footer";
-import Chatbot from "./components/Chatbot";
+import ChatBot from "./components/ChatBot";
 import "./styles.css";
 
 export default function App() {
@@ -20,7 +20,7 @@ export default function App() {
       <Algorithms />
       <Stats />
       <Footer />
-      <Chatbot />
+      <ChatBot />
     </main>
   );
 }
